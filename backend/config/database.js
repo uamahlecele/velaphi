6d893cb74fef0a8207c11a1f505aae134d660bb1
amceljhb025 <amceljhb025@student.wethinkcode.co.za>
2026-06-03 12:29:37 +0200

    feat: server.js implementation and routing. EJS installation for server-side rendering of data in mysql database.

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,3 +1,4 @@
+// Imports
 require("dotenv").config();
 const mysql = require("mysql2");
 
@@ -7,11 +8,10 @@ const pool = mysql.createConnection({
     user: "root",
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    connectionLimit: 10
-});
+
+}).promise() // using the promise api for mySQL. 
 
 // Connection is verified
-
 pool.connect((err) => {
     if (err) {
         console.log("Connection failed:", err);
@@ -22,15 +22,32 @@ pool.connect((err) => {
 
     // Query runs AFTER connection is established
 
-    pool.query("SELECT * FROM surnames", (err, res) => {
-        if (err) {
-            console.log("Query failed:", err);
-            return;
-        }
-        console.log(res);
-    });
+    // pool.query("SELECT * FROM surnames", (err, res) => {
+    //     if (err) {
+    //         console.log("Query failed:", err);
+    //         return;
+    //     }
+    //     console.log(res);
+    // });
 });
+
+async function getSurname(surname) {
+    const [returnRowOfInformation] = await pool.query(
+        `SELECT * 
+        FROM surnames
+        WHERE isibongo= ? 
+        `, [surname] // We pass in a ? in our query and then pass the actual query value as the second parameter seperately so that we prevent SQL Injections
+    )
+    return returnRowOfInformation[0];
+}
+
+// Quick async function that allows me to use await
+
+// (async () => {
+//     const surname = await getSurname('Dladla');
+//     console.log(surname);
+// })();
 
 // Exporting connection for other modules to use
 
-module.exports = pool;
+module.exports = { getSurname };
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,61 @@
-// this will be for expressjs, building my server
+
+/**
+ * This file will be my server and its responsibility is to route requests
+ *  with the corresponding actions
+ * 
+ * To build my server I'll make use of Express.js (Web Framework. Think Flask in python)
+ */
 
 // 1. Express.js setup
+const ejs = require('ejs')
+require("dotenv").config({ quiet: true });
+const path = require('path');
+
+
 const express = require('express');
+const dbConnection = require('./config/database.js');
 const app = express();
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+/**  Sets EJS as the view engine 
+ * A view engine is a software component that allows 
+ * the rendering of dynamic content onto a web page. 
+*/
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 // 2. Defining my routes
 
+//this is the home directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
 app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/about.html'); // This returns the about.html
+    res.sendFile(path.join(__dirname, 'about.html')); // This returns about.html
+});
+
+/** 
+ * This line below is responsible for generating the html file built 
+ * from the data in the surname found
+ */
+
+app.get('/surname', async (req, res) => { //:isibongo is the placeholder for the actual surname
+    const surname = req.query.inputSearch // e.g cele will be stored in the variable surname
+    const foundSurname = await dbConnection.getSurname(surname)
+
+    const { id, isibongo, izithakazelo, umlando, well_known_people } = foundSurname;
+    res.render('index', {
+        surname: isibongo,
+        izithakazelo: izithakazelo,
+        umlando: umlando,
+        famous: well_known_people
+    });
+
 });
 
 
-app.listen(process.env.PORT);
-console.log("Running!");
+app.listen(process.env.PORT, () => {
+    console.log("PORT IS RUNNING!")
+});
+
